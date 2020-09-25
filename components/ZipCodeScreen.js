ad9dbc20244cb425f8ea04ecb7b39ac7394c28de
s613551022 <s6135512022@phuket.psu.ac.th>
2020-09-25 11:23:09 +0700

    9

--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -13,7 +13,7 @@ const availableZipItems = [
 
 const ZipItem = ({ place, code, navigation }) => (
     <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })}>
-        <View style={{ borderColor: 'red', borderWidth: 3 }}>
+        <View style={{ borderColor: 'blue', borderWidth: 3 }}>
             <Text>{place}</Text>
             <Text>{code}</Text>
         </View>
